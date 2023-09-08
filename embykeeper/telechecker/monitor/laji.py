b81e5f705f5ee51c7cb431e19683970092e93391
Cesar <cesaryuan@qq.com>
2023-09-08 02:46:14 +0000

    change laji to server 2

--- embykeeper/telechecker/monitor/laji.py
+++ embykeeper/telechecker/monitor/laji.py
@@ -8,7 +8,7 @@ class LajiMonitor(Monitor):
     chat_keyword = r"[a-zA-Z0-9]{21}"
     # chat_keyword = r"hWVsG1Yadnchd82c7JX5O"
     # chat_keyword = r"RJU1PGzlBXvQCjX1dFBys"
-    bot_username = "zckllflbot"
+    bot_username = "gtlx_bot"
     # chat_name = "-965261929"
     chat_name = "-1001549608772"
     notify_create_name = True
@@ -16,5 +16,5 @@ class LajiMonitor(Monitor):
 
     async def on_trigger(self, message: Message, key, reply):
         await self.client.send_message(self.bot_username, f"/key {key}")
-        await self.client.send_message(self.bot_username, self.unique_name)
+        # await self.client.send_message(self.bot_username, self.unique_name)
         self.log.bind(notify=True).info(f'已向垃圾影音Bot @{self.bot_username} 发送邀请码: "{key}", 请查看.')
