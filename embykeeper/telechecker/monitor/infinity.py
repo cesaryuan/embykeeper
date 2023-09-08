a2e6d295a7239b9a99859242664f1a1b4df45cb6
Cesar <cesaryuan@qq.com>
2023-09-08 02:45:58 +0000

    add monitor for infinity

---
+++ embykeeper/telechecker/monitor/infinity.py
@@ -0,0 +1,20 @@
+from pyrogram.types import Message
+
+from .base import Monitor
+
+
+class InfinityMonitor(Monitor):
+    name = "infinity"
+    chat_keyword = r"register-[-\w]{36}"
+    # chat_keyword = r"hWVsG1Yadnchd82c7JX5O"
+    # chat_keyword = r"RJU1PGzlBXvQCjX1dFBys"
+    bot_username = "Infinity94bot"
+    # chat_name = "-965261929"
+    chat_name = "infinityflyemby"
+    notify_create_name = True
+    allow_edit = False
+
+    async def on_trigger(self, message: Message, key, reply):
+        await self.client.send_message(self.bot_username, f"/invite {key}")
+        await self.client.send_message(self.bot_username, f"/create {self.unique_name}")
+        self.log.bind(notify=True).info(f'已向 infinity Bot @{self.bot_username} 发送邀请码: "{key}", 请查看.')
